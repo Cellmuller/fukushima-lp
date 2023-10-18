0506f8a649b811d68178a91e80b30efb9a0992a0
Ishikawa202 <129378909+Ishikawa202@users.noreply.github.com>
2023-10-18 14:42:50 +0900

    jsアップデート

--- main.js
+++ main.js
@@ -1,43 +1,49 @@
+// Initialize ScrollMagic Controller
+const controller = new ScrollMagic.Controller();
 
-gsap.registerPlugin(ScrollTrigger);
-
-// アニメーション設定を関数化
+// slideFromSide function using ScrollMagic and GSAP
 function slideFromSide(selector, distance, duration) {
-  gsap.from(selector, {
+  let tween = gsap.from(selector, {
     x: distance,
     opacity: 0,
-    duration: duration,
-    scrollTrigger: {
-      trigger: selector,
-      start: "top 80%",
-      toggleActions: "play none none reverse",
-      markers: false,
-    }
+    duration: duration
   });
+  
+  new ScrollMagic.Scene({
+    triggerElement: selector,
+    triggerHook: 0.8, // corresponds to "top 80%"
+    reverse: true // to ensure the animation plays in reverse when scrolling up
+  })
+  .setTween(tween)
+  .addTo(controller);
 }
 
+// fadeInFromBelow function using ScrollMagic and GSAP
 function fadeInFromBelow(selector, duration, delayMultiplier) {
   document.querySelectorAll(selector).forEach((element, index) => {
-    gsap.from(element, {
+    let tween = gsap.from(element, {
       opacity: 0,
       y: 50,
       duration: duration,
-      delay: index * delayMultiplier,
-      scrollTrigger: {
-        trigger: element,
-        start: "top 80%",
-        end: "top 30%",
-        scrub: false,
-        markers: false,
-      }
+      delay: index * delayMultiplier
     });
+
+    new ScrollMagic.Scene({
+      triggerElement: element,
+      triggerHook: 0.8, // corresponds to "top 80%"
+      reverse: true // to ensure the animation plays in reverse when scrolling up
+    })
+    .setTween(tween)
+    .addTo(controller);
   });
 }
-
-// aboutエリアアニメーション
-slideFromSide(".about-area .about-detail:not(.reverse)", 20, 2); // 右から左へのスライドイン
-slideFromSide(".about-area .about-detail.reverse", -20, 2); // 左から右へのスライドイン
 
 // a要素ごとのアニメーション
 fadeInFromBelow(".know-detail a", 1, 0.1);
 fadeInFromBelow(".product-list a", 0.7, 0.1);
+
+
+
+// aboutエリアアニメーション
+slideFromSide(".about-area .about-detail:not(.reverse)", 20, 2); // 右から左へのスライドイン
+slideFromSide(".about-area .about-detail.reverse", -20, 2); // 左から右へのスライドイン
